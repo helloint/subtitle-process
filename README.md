b02eb7c567894e76532c8a13d2243d053f895bbc
Wayne Mao <wayne.mao@neulion.com.cn>
2021-07-24 22:03:04 +0800

    feat: integrate srt function

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # 给英语视频添加双语字幕的一个解决方案
 ## 命令说明
-### 导出(Extract)
-npm环境`npm run extract`, 或者node环境, `node extract.js`, 字幕文件会提取到 translation.txt
-### 导入(Merge)
-npm环境`npm run merge`, 或者node环境, `node merge.js`, 翻译文件 translated.txt 会被追加到已有的英文字幕后(换行显示)
+### 导出 (Extract)
+npm环境`npm run extract`, 或者node环境, `node extract.js`, 字幕文件会提取到 translation.txt  
+### 合并 (Merge)
+npm环境`npm run merge`, 或者node环境, `node merge.js`, 翻译文件 translated.txt 会被追加到已有的英文字幕后(换行显示)  
+### 生成字幕文件 (Srt)
+npm环境`npm run srt`, 或者node环境, `node srt.js`, 导出srt字幕文件  
 
 ## How To Use
 ### 准备
 0. 环境配置：  
 安装【node/npm环境】  
 安装【剪映[桌面端](https://lv.ulikecam.com/)】  
-安装【JianYingSRT 剪映提取导出转换为SRT字幕工具】  
 安装【[FFmpeg](http://ffmpeg.org/download.html)】    
 1. 在剪映里导入原视频，通过剪映自动生成英语字幕(菜单/文本/识别字幕)。退出剪映。
 2. 把剪映草稿的配置文件($Users\AppData\Local\JianyingPro\User Data\Projects\com.lveditor.draft)`draft_content.json`复制到当前目录，运行【导出】待翻译文件 `translation.txt`
-3. 用 Google Translation 翻译 `translation.txt`，得到 `translated.txt`。 注意比对行数，确保不要错位。
-4. 运行【导入】 合并成 `draft_content_imported.json`，覆盖剪映源文件。
-5. 打开SRT字幕工具，选择剪映草稿，导出为SRT字幕`subtitle.srt`。（TODO: 这部可以集成进自动化）
+3. 用 Google Translation 翻译 `translation.txt`，得到 `translated.txt`。 注意比对行数，确保内容不要错位。
+4. 运行【导入】 合并成 `draft_content_imported.json`。
+4. 运行【生成字幕文件】 生成 `subtitle.srt`。
 6. 通过FFmpeg合成字幕，同时压缩视频。  
 `ffmpeg -y -i src.mp4 -lavfi "subtitles=subtitle.srt" -c:v h264 -rc constqp -qp 25 dist.mp4`
---
+++ srt.js
@@ -0,0 +1,79 @@
+// file content format:
+// 1
+// 00:00:18,580 --> 00:00:23,347
+// 很久以前在遙遠的銀河系...
+//
+// 2
+// 00:00:26,164 --> 00:00:31,124
+// 星際大戰
+// "target_timerange": {
+// 	"duration": 2333333,
+// 			"start": 7433333
+// },
+//
+
+// draft_content_imported.json -> subtitle.srt
+const fs = require('fs');
+const jyData = JSON.parse(fs.readFileSync('draft_content_imported.json', 'utf8'));
+const srtFile = 'subtitle.srt';
+
+let srtData = [];
+var texts = jyData.materials.texts;
+var tracks = {};
+for (var i = 0; i < jyData.tracks.length; i++) {
+	if (jyData.tracks[i].type === 'text') {
+		for (var j = 0; j < jyData.tracks[i].segments.length; j++) {
+			// Sample Data:
+			// "target_timerange": {
+			// 	"duration": 24933333,
+			// 	"start": 0
+			// },
+			// format: 00:00:18,580 --> 00:00:23,347
+			var timestamp = [
+				formatTime(jyData.tracks[i].segments[j].target_timerange.start),
+				' --> ',
+				formatTime(jyData.tracks[i].segments[j].target_timerange.start + jyData.tracks[i].segments[j].target_timerange.duration)
+			].join('');
+
+			tracks[jyData.tracks[i].segments[j].material_id] = timestamp;
+		}
+	}
+}
+for (var i = 0; i < texts.length; i++) {
+	srtData[srtData.length] = i + 1;
+	srtData[srtData.length] = tracks[texts[i].id];
+	srtData[srtData.length] = texts[i].content;
+	srtData[srtData.length] = '';
+}
+srtData[srtData.length] = '';
+
+fs.writeFileSync(srtFile, srtData.join('\n'), 'utf8');
+
+// 80500000 -> 00:01:20,500
+function formatTime(time) {
+	var h = Math.floor(time / 3600000000);
+	var m = Math.floor((time - h * 3600000000) / 60000000);
+	var s = Math.floor((time - h * 3600000000 - m * 60000000) / 1000000);
+	var ms = Math.floor((time - h * 3600000000 - m * 60000000 - s * 1000000) / 1000);
+	var ret = [
+		[
+			fillZero(h + '', 2),
+			fillZero(m + '', 2),
+			fillZero(s + '', 2),
+		].join(':'),
+		fillZero(ms + '', 3),
+	].join(',');
+
+	return ret;
+}
+
+// '2', 3 -> '002'
+function fillZero(str, length) {
+	if (str.length >= length) return str;
+
+	while (str.length < length) {
+		str = '0' + str;
+	}
+
+	return str;
+}
